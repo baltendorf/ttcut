ff86b4ad1e7e9f145baa913e4cf351899a79111f
b. altendorf <b.altendorf@tritime.de>
2010-05-26 21:23:38 +0200

    * formatted

--- common/ttthreadtask.h
+++ common/ttthreadtask.h
@@ -40,55 +40,56 @@
 class TTMessageLogger;
 
 //! Baseclass for all task runables
+
 class TTThreadTask : public QObject, public QRunnable
 {
-	Q_OBJECT
+  Q_OBJECT
 
-	public:
-		TTThreadTask(QString name);
-    virtual ~TTThreadTask();
+public:
+  TTThreadTask(QString name);
+  virtual ~TTThreadTask();
 
-		void    run();
-    void    runSynchron();
-    QString taskName() const;
-    QUuid   taskID() const;
-    int     elapsedTime() const;
-    quint64 totalSteps() const;
-    quint64 stepCount() const;
-    int     processValue() const;
-    bool    isRunning() const;
-    bool    isAborted() const;
-    void    setIsRunning(bool value);
+  void run();
+  void runSynchron();
+  QString taskName() const;
+  QUuid taskID() const;
+  int elapsedTime() const;
+  quint64 totalSteps() const;
+  quint64 stepCount() const;
+  int processValue() const;
+  bool isRunning() const;
+  bool isAborted() const;
+  void setIsRunning(bool value);
 
-  protected:
-    virtual void operation() = 0;
-    virtual void cleanUp() = 0;
-    virtual void abort();
+protected:
+  virtual void operation() = 0;
+  virtual void cleanUp() = 0;
+  virtual void abort();
 
-	public slots:
-		virtual void onUserAbort() = 0;
+public slots:
+  virtual void onUserAbort() = 0;
 
-  protected slots:
-    virtual void onStatusReport(int state, const QString& msg, quint64 value);
-    virtual void onStatusReport(TTThreadTask* task, int state, const QString& msg, quint64 value);
+protected slots:
+  virtual void onStatusReport(int state, const QString& msg, quint64 value);
+  virtual void onStatusReport(TTThreadTask* task, int state, const QString& msg, quint64 value);
 
-	signals:
-	  void started(TTThreadTask* task);   /**<internal signal thread was started  */
-	  void finished(TTThreadTask* task);  /**<internal signal thread has finished */
-	  void aborted(TTThreadTask* task);   /**<internal signal thread was aborted  */
+signals:
+  void started(TTThreadTask* task); /**<internal signal thread was started  */
+  void finished(TTThreadTask* task); /**<internal signal thread has finished */
+  void aborted(TTThreadTask* task); /**<internal signal thread was aborted  */
 
-		void statusReport(TTThreadTask* task, int state, const QString& msg, quint64 value);
+  void statusReport(TTThreadTask* task, int state, const QString& msg, quint64 value);
 
-	protected:
-		QUuid            mTaskID;      /**<unique task ID                             */
-    QTime            mTaskTime;    /**<time set to current time when task started */
-    quint64          mTotalSteps;  /**<estimate count of total task steps         */
-    quint64          mStepCount;   /**<current step count                         */
-    TTMessageLogger* log;          /**<message logger istance                     */
-    QString          mTaskName;    /**<task name                                  */
-    bool             mIsSynchron;
-    bool             mIsRunning;
-    bool             mIsAborted;
+protected:
+  QUuid mTaskID; /**<unique task ID                             */
+  QTime mTaskTime; /**<time set to current time when task started */
+  quint64 mTotalSteps; /**<estimate count of total task steps         */
+  quint64 mStepCount; /**<current step count                         */
+  TTMessageLogger* log; /**<message logger istance                     */
+  QString mTaskName; /**<task name                                  */
+  bool mIsSynchron;
+  bool mIsRunning;
+  bool mIsAborted;
 };
 
 #endif
